2d009d276c9c54f488a81dc120d7a8efad843ca4
Sab Ayce <s.ayache@sciencesu-lyon.fr>
2020-07-21 09:47:28 +0200

    tyui

--- src/CollaborateurBundle/Controller/DefaultController.php
+++ src/CollaborateurBundle/Controller/DefaultController.php
@@ -93,10 +93,12 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $collab = $em->getRepository('CollaborateurBundle:Collab')->find($id);
+        $collabs = $this->getDoctrine()->getManager()->getRepository('CollaborateurBundle:Collab')->findAll();
         $collabForm = $this->createForm(CollabType::class, $collab);
 
         return $this->render('detailscollab.html.twig', [
-            'form' => $collabForm->createView()
+            'form' => $collabForm->createView(),
+            'collabs' => $collabs
         ]);
     }
 
